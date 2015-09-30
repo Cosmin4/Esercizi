c1ebd7965a4a4f77fb2c9f30e4fc40a3fa1c0fe8
cosmin dirlea <cdirlea4@gmail.com>
2015-09-30 22:13:45 +0200

    Problema risolto, ora restituisce(sbagliato utilizzo del costruttore) manca utilizzo LinQ.

--- Classi/Classi/Program.cs
+++ Classi/Classi/Program.cs
@@ -11,14 +11,19 @@ namespace Classi
         static void Main(string[] args)
         {
             var listaPersone = new List<Persona>();
-            new Persona("Pippo", 800, new DateTime(2000, 2, 23));
-            new Persona("Astolfo", 1500, new DateTime(1996, 12, 18));
+            var p1 = new Persona("Logitech", 900, new DateTime(2000, 2, 23));
+            listaPersone.Add(p1);
+            var p2 = new Persona("Epson", 1200, new DateTime(1998, 4, 15));
+            listaPersone.Add(p2);
+            var p3 = new Persona("Jeck", 1500, new DateTime(1996, 12, 18));
+            listaPersone.Add(p3);
+            new Persona("Astolfo", 1500, new DateTime(1996, 12, 18)); //questo non lo restiuisce, mentre quello sopra si.
 
             foreach (var Persona in listaPersone)
             {
-                Console.WriteLine(Persona.ToString() + "\n");
-                Console.ReadKey();
+                Console.WriteLine(Persona.ToString() + "\n");  
             }
+            Console.ReadKey();
         }
     }
 }
